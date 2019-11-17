954013c37018ebb31783317d3ca3b660a4ea9a23
elenaminyaeva <alu0101251981@ull.edu.es>
2019-11-17 22:05:59 +0000

    gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,37 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat-css');
+var browserSync = require('browser-sync').create();
+var sourcemaps = require('gulp-sourcemaps');
+let cleanCSS = require('gulp-clean-css');
+
+gulp.task('minifyCss', () => {
+  return gulp
+    .src('src/styles/*.css')
+    .pipe(cleanCSS({ compatibility: 'ie8' }))
+    .pipe(gulp.dest('dist'));
+});
 
 gulp.task('concat', function() {
   return gulp
     .src(['src/styles/main.css','src/styles/minor.css'])
     .pipe(concat('concat.css'))
-    .pipe(gulp.dest('styles/dist'));
+    .pipe(gulp.dest('dist'));
+});
+ 
+gulp.task('browser-sync', function() {
+browserSync.init({
+server: {
+baseDir: "/home/usuario/práctica_1"
+}
+});
 });
 
-
+gulp.task('sourcesMaps', function(done) {
+  gulp
+    .src('src/styles/*.css')
+    .pipe(sourcemaps.init())
+    .pipe(cleanCSS({ compatibility: 'ie8' }))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('dist/css'));
+  done();
+});
